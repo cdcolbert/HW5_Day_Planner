9695d04e261f2623e30617b7e066ab433970857c
Corey Colbert <cdcolbert10@gmail.com>
2021-01-24 11:22:02 -0600

    Initial upload of Homework5 files.

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var timeArray = ["9:00 AM","10:00 AM","11:00 AM", "12:00 PM", "1:00 PM","2:00 PM","3:00 PM","4:00 PM","5:00 PM"];
+var tasksArray = [];
+var dateTime = moment().format('llll');
+
+
+$("#currentDay").text(dateTime);
+
+
