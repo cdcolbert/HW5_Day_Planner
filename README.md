72c166cbdb8b83878cb7d7bb2499d92a04fd83a1
Corey Colbert <cdcolbert10@gmail.com>
2021-01-24 18:02:44 -0600

    Updated README, JS, and CSS

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # HW5_Day_Planner
+# Project Name
+> Simple Day Planner
+
+## Table of contents
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [Features](#features)
+* [Status](#status)
+* [Contact](#contact)
+
+## General info
+This project is a day planner app using JavaScript.  The app displays the current date and time, and alllows the user to enter tasks for the time slots.  The user input is stored in local storage and remains on the page when it is refreshed.
+
+
+
+## Code Examples
+Examples of usage:
+`
+
+  `
+
+## Features
+List of features ready and TODOs for future development
+* 
+* 
+
+TODO:
+* 
+* 
+
+## Status
+Release 1.0
+
+Project is: _unfinished_ Project was designed as a homework assignment for a boot camp class.  This is the submission for the assignment.
+
+## Contact
+Created by [Corey Colbert](cdcolbert10@gmail.com) - feel free to contact me!
--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 
-var timeArray = ["9:00 AM","10:00 AM","11:00 AM", "12:00 PM", "1:00 PM","2:00 PM","3:00 PM","4:00 PM","5:00 PM"];
-var tasksArray = [];
-var dateTime = moment().format('llll');
+$(document).ready(function() {
+
+    var timeArray = ["9:00 AM","10:00 AM","11:00 AM", "12:00 PM", "1:00 PM","2:00 PM","3:00 PM","4:00 PM","5:00 PM"];
+    var tasksArray = [];
+    var dateTime = moment().format('llll');
+
+    $("#currentDay").text(dateTime);
 
 
-$("#currentDay").text(dateTime);
+    init();
 
+    function init() {        
+        createTimeSlots();
+    }
+    
+    function createTimeSlots(){
+
+        for (var i = 0; i < timeArray.length; i++) {
+            var hourIndex = i;
+            var timeSlot = $("<div></div>").addClass("time-block row");
+            var timeDisplay = $("<div></div>").addClass("hour col-2").text(timeArray[hourIndex]);
+            var taskText = $("<textarea></textarea>").addClass("input" + hourIndex + " description col-9 border-top border-bottom").text("");
+            var button = $("<button></button>").addClass("saveBtn col-1").attr("data-btnIndex", hourIndex).text("Save");
+            
+            $(".container").append(timeSlot);
+            timeSlot.append(timeDisplay,taskText,button);
+        }
+    }
+
+    
+
+
+});
 
